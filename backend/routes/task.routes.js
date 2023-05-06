d76bb98334b411c1e37d87f5e5f62a07bf62b0e4
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-05-06 15:00:02 +0200

    routes

---
+++ backend/routes/task.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {
+	setTasks,
+	getTasks,
+	editTask,
+	deleteTask,
+} = require('../controllers/task.controller');
+const router = express.Router();
+
+router.get('/', getTasks);
+router.post('/', setTasks);
+router.put('/:id', editTask);
+router.delete('/:id', deleteTask);
+
+module.export = router;
