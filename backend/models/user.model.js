e04042aa43b4d29e9f795af3e3c909ddac25e9e1
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-05-09 15:38:28 +0200

    middleware pre-save

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 const userSchema = mongoose.Schema({
 	name: {
 		type: String,
@@ -14,5 +16,22 @@ const userSchema = mongoose.Schema({
 		required: true,
 	},
 });
+
+userSchema.pre('save', async (next) => {
+	if (!this.isModified('password')) {
+		return next();
+	}
+	try {
+		const salt = await bcrypt.genSalt(10);
+		this.password = await bcrypt.hash(this.password, salt);
+		next();
+	} catch (err) {
+		next(err);
+	}
+});
+
+userSchema.methods.comparePasswords = async (candidatePassword) => {
+	return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('user', userSchema);
