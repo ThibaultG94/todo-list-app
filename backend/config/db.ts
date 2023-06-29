5e5fe906b0b3a36f845a7d86a07cf5fc1d7338d1
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-06-29 10:54:46 +0200

    db.ts Comments + adjustments

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,19 +1,33 @@
 import mongoose from 'mongoose';
 
+/**
+ * Etablishes a connection with the MongoDB database.
+ * The function will attempt to connect to a test database if the environment variable
+ * NODE_ENV is set to 'test'. Otherwise, it will attempt to connect to the development database.
+ */
+
 export const connectDB = async () => {
 	try {
-		mongoose.set('strictQuery', false);
-		const dbConnection: any =
+		// Determine the correct database to connect to.
+		const dbConnection: string =
 			process.env.NODE_ENV === 'test'
 				? process.env.TEST_MONGO_URI
 				: process.env.MONGO_URI;
-		const state = process.env.NODE_ENV === 'test' ? 'test' : 'development';
+
+		// Attempt to connect to the database.
 		await mongoose.connect(dbConnection);
+
+		// Log the successful connection and the current state of development.
 		console.log(`Succesfully connected to ${dbConnection}`);
-		console.log(`Etat : ${state}`);
+		console.log(
+			`Etat : ${process.env.NODE_ENV === 'test' ? 'test' : 'development'}`
+		);
 	} catch (err) {
+		// If the connection attempt failed, log the error.
 		const result = (err as Error).message;
 		console.error('Error connecting to MongoDB', result);
+
+		// Exit the process with a failure code.
 		process.exit(1);
 	}
 };
