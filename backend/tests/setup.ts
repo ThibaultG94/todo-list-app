09c1983dc6cd88035708323aeb0fa28c5d5f1038
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-07-03 11:57:59 +0200

    setup.ts comments and corrections

--- backend/tests/setup.ts
+++ backend/tests/setup.ts
@@ -1,18 +1,25 @@
-const mongoose = require('mongoose');
+import mongoose, { MongooseError } from 'mongoose';
 
+// Setting mongoose Promise to use Node.js native Promise
 mongoose.Promise = Promise;
+
+// Define the MongoDB URI
 const mongoUri = 'mongodb://localhost:27017/tests/';
 
+// Connect to the MongoDB instance
 mongoose.connect(mongoUri);
 
-mongoose.connection.on('error', (e: any) => {
-	if (e.message.code === 'ETIMEDOUT') {
-		console.log(e);
+// Log an error if there is one
+mongoose.connection.on('error', (e: MongooseError) => {
+	const error = e as any;
+	if (error.code === 'ETIMEDOUT') {
+		console.log('ETIMEDOUT error occured', e);
 		mongoose.connect(mongoUri);
 	}
 	console.log(e);
 });
 
+// Log a success message when the database connection is successfully established
 mongoose.connection.once('open', () => {
 	console.log(`MongoDB successfully connected to ${mongoUri}`);
 });
