3ba150b6864280784b1a094ba97264e5436e8399
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-07-03 11:06:35 +0200

    validation.middlewares.ts comments and corrections

--- backend/middlewares/validation.middlewares.ts
+++ backend/middlewares/validation.middlewares.ts
@@ -1,12 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 
-export const validateUserID = async (
+// Middleware to validate user ID
+export const validateUserID = (
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) => {
-	const userId = await req.params.id;
-	await console.log(userId);
+	// Get user ID from request params
+	const userId = req.params.id;
+
+	// Check if user ID is a valid Mongo ObectID
 	if (!/^[0-9a-fA-F]{24}$/.test(userId)) {
 		return res.status(400).send({ error: 'Invalid user ID' });
 	} else {
@@ -14,17 +17,21 @@ export const validateUserID = async (
 	}
 };
 
-export const validatePageAndLimit = async (
+// Middleware to validate pagination parameters
+export const validatePageAndLimit = (
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) => {
-	const { page, limit } = await req.query;
-	await console.log(req.query);
+	// Get page and limit from request query
+	const { page, limit } = req.query;
+
+	// Check if page number is a positive integer
 	if (page && (!Number.isInteger(+page) || +page <= 0)) {
 		return res.status(400).send({ error: 'Invalid page number' });
 	}
 
+	// Check if limit number is a positive integer
 	if (limit && (!Number.isInteger(+limit) || +limit <= 0)) {
 		return res.status(400).send({ error: 'Invalit limit number' });
 	}
