64b8ad913ce9ca981a5bf8a70ca678b34b4782c0
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-07-05 22:44:05 +0200

    register form

--- frontend/js/page/home.js
+++ frontend/js/page/home.js
@@ -1,7 +1,9 @@
 import login from '../components/login.js';
 import modalRegister from '../components/modal.js';
+import register from '../components/register.js';
 
 export default function home() {
 	login();
 	modalRegister();
+	register();
 }
