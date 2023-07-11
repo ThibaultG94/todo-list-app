84a8b4ee9e2eb577e07cecbc601466f379ed9d9f
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-07-11 20:40:08 +0200

    nav.js

--- frontend/js/components/nav.js
+++ frontend/js/components/nav.js
@@ -1,3 +1,27 @@
 export default function nav() {
-	console.log('navbar');
+	const homeLink = document.getElementById('home');
+	const tasksLink = document.getElementById('tasks');
+	const listLink = document.getElementById('list');
+	const boardLink = document.getElementById('board');
+	const calendarLink = document.getElementById('calendar');
+
+	homeLink.addEventListener('click', () => {
+		window.location.href = '/frontend/pages/home.html';
+	});
+
+	tasksLink.addEventListener('click', () => {
+		window.location.href = '/frontend/pages/taskList.html';
+	});
+
+	listLink.addEventListener('click', () => {
+		window.location.href = '/frontend/pages/taskList.html';
+	});
+
+	boardLink.addEventListener('click', () => {
+		window.location.href = '/frontend/pages/taskBoard.html';
+	});
+
+	calendarLink.addEventListener('click', () => {
+		window.location.href = '/frontend/pages/taskCalendar.html';
+	});
 }
