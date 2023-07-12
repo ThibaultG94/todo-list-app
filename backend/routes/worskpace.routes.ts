79e84ac91d662c0a5f839253bf465fea0b60b59b
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-07-12 19:27:55 +0200

    workspace middleware

--- backend/routes/worskpace.routes.ts
+++ backend/routes/worskpace.routes.ts
@@ -7,6 +7,7 @@ import {
 	getUserWorkspaces,
 	getWorkspace,
 } from '../controllers/workspace.controller';
+import { validateUserID } from '../middlewares/validation.middlewares';
 
 const router = express.Router();
 
@@ -14,15 +15,15 @@ const router = express.Router();
 router.post('/', auth, createWorkspace);
 
 // Route to get a workspace by its id
-router.get('/:id', auth, getWorkspace);
+router.get('/:id', auth, validateUserID, getWorkspace);
 
 // Route to get all workspaces for a specific user
-router.get('/user/:id', auth, getUserWorkspaces);
+router.get('/user/:id', auth, validateUserID, getUserWorkspaces);
 
 // Route to update a workspace by its id
-router.put('/:id', auth, editWorkspace);
+router.put('/:id', auth, validateUserID, editWorkspace);
 
 // Route to delete a workspace by its id
-router.delete('/:id', auth, deleteWorkspace);
+router.delete('/:id', auth, validateUserID, deleteWorkspace);
 
 export default router;
